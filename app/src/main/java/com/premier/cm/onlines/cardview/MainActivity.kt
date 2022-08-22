da7c14b7108209e91b8862ff85362ecb036e5d24
wallace08797 <fenilahir16@gmail.com>
2022-08-22 16:53:38 +0530

    Initial commit

---
+++ app/src/main/java/com/premier/cm/onlines/cardview/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.premier.cm.onlines.cardview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val card1:CardView=findViewById(R.id.c1)
+
+        card1.setOnClickListener{
+            Toast.makeText(this,"you have selected adding a new Task",Toast.LENGTH_LONG).show()
+        }
+    }
+}
